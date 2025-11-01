ab875c4d7d5c60995fa824816bc06dc3bbd65044
alvinkabwama <alkaleos10@gmail.com>
2025-10-31 19:32:38 -0500

    Removed secrets that were accidentally commited

--- test_mongodb.py
+++ test_mongodb.py
@@ -5,7 +5,7 @@ import dotenv
 
 
 
-uri = "mongodb+srv://alvinkabwama10:Cleophus10@cluster0.vbgxpkj.mongodb.net/?appName=Cluster0"
+
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
